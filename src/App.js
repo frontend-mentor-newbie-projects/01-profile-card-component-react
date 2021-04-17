e9bdb3795993805c48a992b1ee79f4d585f97a1b
likelytwitchdollop <cynthiamahofa@yahoo.com>
2021-04-17 23:46:44 +0200

    Reset browser defaults

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
 import React from 'react'
+import styled, { createGlobalStyle } from 'styled-components'
+
+const GlobalStyles = createGlobalStyle`
+    html {
+        box-sizing: border-box;
+    }
+
+    *, ::after, ::before {
+        box-sizing: inherit;
+    }
+
+    body,
+    h1, h2, h3, h4, h5, h6,
+    p, a, ul, li,
+    img {
+        margin: 0;
+        padding: 0;
+        text-decoration: none;
+    }
+
+    li {
+        list-style-type: none;
+    }
+`
 
 function App() {
     return (
-        <div>
+        <>
+            <GlobalStyles />
             Victor Crest         
             26
             London
@@ -15,7 +40,7 @@ function App() {
 
             1.4K
             Photos
-        </div>
+        </>
     )
 }
 
